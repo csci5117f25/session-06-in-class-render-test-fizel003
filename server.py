a4dacb6989bfff88d03023ac4163af2dfe9bdefa
fizel003 <fizel003@umn.edu>
2025-09-25 08:12:34 -0500

    Added guestbook functionality

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
@@ -6,3 +6,13 @@ app = Flask(__name__)
 @app.route('/<name>')
 def hello(name=None):
     return render_template('hello.html', name=name)
+
+writings = []
+
+@app.route('/guestbook', methods=['GET'])
+def hello_world3():
+    name = request.args.get("name", "")
+    message = request.args.get("message", "")
+    writings.append([name, message])
+    print(writings)
+    return render_template('hello.html', writings=writings)
